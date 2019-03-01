fac7dde1cebff84ef1baa906cae5e76fae94a9c5
Oh Xyz <ohxyzcom@gmail.com>
2019-03-01 16:40:15 +1100

    Removed Minton themes

---
+++ public/assets/scripts.js
@@ -0,0 +1,11 @@
+( () => {
+
+    let $mobileMenuButton = $( '.mobile-menu__button' );
+    let $container = $( '.container' );
+
+    $mobileMenuButton.click( () => { 
+
+        $container.toggleClass( 'container--mobile' );
+    } )
+
+} )();
