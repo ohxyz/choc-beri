d130538199291b37f07c6d409d9052927ec7af5f
Oh Xyz <ohxyzcom@gmail.com>
2019-03-06 15:47:37 +1100

    Done most of the features status page

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,11 +1,52 @@
-( () => {
+( function () {
 
-    let $mobileMenuButton = $( '.mobile-menu__button' );
-    let $container = $( '.container' );
+    var $mobileMenuButton = $( '.mobile-menu__button' );
+    var $container = $( '.container' );
 
-    $mobileMenuButton.click( () => { 
+    $mobileMenuButton.click( function () { 
 
         $container.toggleClass( 'container--mobile' );
+    } );
+
+    $( '.feature-status__icon-expand' ).click( function () {
+
+        var $icon = $( this );
+        var $content = $icon.parent().next();
+
+        if ( $icon.hasClass( 'fa-plus') ) {
+
+            $icon.removeClass( 'fa-plus' ).addClass( 'fa-minus' );
+            $content.hide();
+        }
+        else if (  $icon.hasClass( 'fa-minus') ) {
+
+            $icon.removeClass( 'fa-minus' ).addClass( 'fa-plus' );
+            $content.show();
+        }
+        
+    } );
+
+    $( '.strip__feature-store-name' ).click( function () { 
+
+        var $button = $( this );
+        var text = $button.text();
+
+        if ( text === 'all' ) {
+
+            $( '.feature-store' ).show();
+            $( '.feature-store__header' ).show();
+        }
+        else {
+
+            $( '.feature-store' ).hide();
+            $( '.feature-store__header' ).hide();
+            $( '.strip__feature-store-name' ).removeClass( 'strip__feature-store-name--active' );
+
+            $button.addClass( 'strip__feature-store-name--active' );
+            $( '#' + text ).show();
+        }
+
+
     } );
 
 } )();
